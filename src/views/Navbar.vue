3d337b8bbee51264be19a700dfa67f1b7dc638ff
Zdravka Strahinova <zdravka.strahinova@programista.pro>
2019-01-14 22:50:57 +0200

    Remove href attributes from navbar

--- src/views/Navbar.vue
+++ src/views/Navbar.vue
@@ -4,10 +4,10 @@
             <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
             <b-collapse is-nav id="nav_collapse">
                 <b-navbar-nav>
-                    <b-nav-item to="/trending" href="#">Vue Cinema</b-nav-item>
+                    <b-nav-item to="/trending">Vue Cinema</b-nav-item>
                 </b-navbar-nav>
                 <b-navbar-nav class="ml-auto">
-                    <b-nav-item to="/trending" href="#">Trending</b-nav-item>
+                    <b-nav-item to="/trending">Trending</b-nav-item>
                     <b-nav-item to="/movies">Movies</b-nav-item>
                     <b-nav-item to="/serials">Serials</b-nav-item>
                     <b-nav-item to="/about">About</b-nav-item>
@@ -16,8 +16,3 @@
         </div>
     </b-navbar>
 </template>
-
-<script>
-    export default {
-    };
-</script>
