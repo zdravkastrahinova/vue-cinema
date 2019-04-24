500dd9b17b033f1c7f694943a684430df1db3065
zdravkastrahinova <zdravkastrahinova1022@gmail.com>
2019-04-24 18:22:32 +0300

    Implement adding movie. Show default image if there is no movie image

--- src/components/movies/Movies.vue
+++ src/components/movies/Movies.vue
@@ -1,35 +1,97 @@
 <template>
     <div>
+        <div class="row mb-4">
+            <h3>Movies</h3>
+        </div>
+
         <div class="row">
-            <div class="col-md-10">
-                <h3>Movies</h3>
-            </div>
-            <div class="col-md-2 text-right">
-                <b-button variant="outline-primary">Add</b-button>
+            <h5>Add New Movie</h5>
+            <div class="col-md-12 pr-0 pl-0">
+                <b-form @submit="onSubmit">
+                    <b-form-group id="movie-title"
+                                  label="Title"
+                                  label-for="movie-title-input">
+                        <b-form-input
+                                id="movie-title-input"
+                                v-model="form.title"
+                                type="text"
+                                required
+                                placeholder="Enter title..."
+                        ></b-form-input>
+                    </b-form-group>
+
+                    <b-form-group id="description"
+                                  label="Description"
+                                  label-for="description-input">
+                        <b-form-input
+                                id="description-input"
+                                v-model="form.description"
+                                type="text"
+                                required
+                                placeholder="Enter description..."
+                        ></b-form-input>
+                    </b-form-group>
+
+                    <b-form-group id="release-date"
+                                  label="Release Date"
+                                  label-for="release-date-input">
+                        <b-form-input
+                                id="release-date-input"
+                                v-model="form.releaseDate"
+                                type="date"
+                                required
+                                placeholder="Enter release date..."
+                        ></b-form-input>
+                    </b-form-group>
+
+                    <b-form-group id="image-path"
+                                  label="Image"
+                                  label-for="image-path-input">
+                        <b-form-input
+                                id="image-path-input"
+                                v-model="form.imagePath"
+                                type="text"
+                                placeholder="Enter image path..."
+                        ></b-form-input>
+                    </b-form-group>
+
+                    <b-button type="submit" variant="primary">Save</b-button>
+                </b-form>
             </div>
         </div>
 
-        <div v-if="!movies || !movies.length" class="text-center">
-            No movies found.
-        </div>
+        <div class="row">
+            <div v-if="!movies || !movies.length" class="text-center">
+                No movies found.
+            </div>
 
-        <div class="row mt-3">
-            <div v-for="movie in movies"
-                 v-bind:key="movie.id"
-                 class="col-md-6">
-                <div class="card flex-row mt-3 movie-container">
-                    <div class="image-container">
-                        <img :src="movie.imagePath"
-                             :alt="movie.title"
-                             class="movie-image"/>
-                    </div>
-                    <div class="movie-info d-flex flex-column p-3">
-                        <h5>{{ movie.title }}</h5>
-                        <p class="text-dark"><span>{{ movie.releaseDate }}</span>
-                        <p class="text-black-50 movie-description">{{ movie.description }}</p>
+            <div class="row mt-3">
+                <div v-for="movie in movies"
+                     v-bind:key="movie.id"
+                     class="col-md-6">
+                    <div class="card flex-row mt-3 movie-container">
+                        <div class="image-container">
+                            <img v-if="movie.imagePath"
+                                 :src="movie.imagePath"
+                                 :alt="movie.title"
+                                 class="movie-image"/>
 
-                        <div class="mt-auto d-flex justify-content-end">
-                            <b-button variant="default" class="btn-details" @click="viewDetails(movie._id)">View details</b-button>
+                            <img v-if="!movie.imagePath"
+                                 src="../../assets/default.jpg"
+                                 alt="default"
+                                 class="movie-image"/>
+                        </div>
+
+                        <div class="movie-info d-flex flex-column p-3">
+                            <h5>{{ movie.title }}</h5>
+                            <p class="text-dark"><span>{{ movie.releaseDate }}</span>
+                            <p class="text-black-50 movie-description">{{ movie.description }}</p>
+
+                            <div class="mt-auto d-flex justify-content-end">
+                                <b-button variant="default" class="btn-details" @click="viewDetails(movie._id)">
+                                    View details
+                                </b-button>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -48,6 +110,12 @@
     </div>
 </template>
 
+<style scoped>
+    .movie-info {
+        width: 100%;
+    }
+</style>
+
 <script>
     import moment from 'moment';
     import movieGraphqlService from '../../api/movie-graphql-service';
@@ -56,27 +124,63 @@
         data() {
             return {
                 movies: [],
-                selectedMovie: {}
+                selectedMovie: {},
+                form: {
+                    title: '',
+                    description: '',
+                    releaseDate: ''
+                }
             }
         },
         created() {
-            movieGraphqlService.getAll((response) => {
-                this.movies = response.data.movies;
-
-                this.movies.forEach(movie => {
-                    movie.releaseDate = moment(movie.releaseDate).format(moment.HTML5_FMT.DATE);
-                });
-
-            }, (error) => {
-                console.log(error);
-            });
+            this.get();
         },
         methods: {
+            get() {
+                movieGraphqlService.getAll((response) => {
+                    this.movies = response.data.movies;
+
+                    this.movies.forEach(movie => {
+                        movie.releaseDate = moment(movie.releaseDate).format(moment.HTML5_FMT.DATE);
+                    });
+
+                }, (error) => {
+                    console.log(error);
+                });
+            },
             viewDetails(_id) {
                 if (_id) {
                     this.selectedMovie = this.movies.find(m => m._id === _id);
                     this.$refs['movie-details-modal'].show();
                 }
+            },
+            resetForm() {
+                this.form = {
+                    title: '',
+                    description: '',
+                    releaseDate: ''
+                }
+            },
+            onSubmit(event) {
+                event.preventDefault();
+
+                const movie = {
+                    title: this.form.title,
+                    description: this.form.description,
+                    releaseDate: this.form.releaseDate
+                };
+
+                movieGraphqlService.create(movie, () => {
+                    this.$toast.success('Movie was successfully saved.', {
+                        position: 'bottom',
+                        duration: 5000
+                    });
+
+                    this.resetForm();
+                    this.get();
+                }, (error) => {
+                    console.log(error);
+                });
             }
         }
     }
--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import VueBootstrap from 'bootstrap-vue';
+import VueToast from 'vue-toast-notification';
 
 import App from './App.vue'
-import './assets/styles/styles.scss';
 import router from "./router";
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'vue-toast-notification/dist/index.css';
+import './assets/styles/styles.scss';
+
 Vue.use(VueBootstrap);
+Vue.use(VueToast);
 
 Vue.config.productionTip = false;
 
